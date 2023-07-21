a4454232e500b62855891b77acb2734f703c6f47
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2023-07-21 15:46:51 +0330

    add editor sections

--- src/lib/editor/Editor.svelte
+++ src/lib/editor/Editor.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+  import EditorSection from '$lib/editor/EditorSection.svelte';
+  import ItemsEditor from '$lib/editor/items-editor/ItemsEditor.svelte';
+  import LayoutEditor from '$lib/editor/layout-editor/LayoutEditor.svelte';
   import Paper from './Paper.svelte';
   import EditorLayout from './EditorLayout.svelte';
 </script>
@@ -7,5 +10,15 @@
   <svelte:fragment slot="preview">
     <Paper>page 1</Paper>
     <Paper>page 2</Paper>
+  </svelte:fragment>
+  <svelte:fragment slot="editor">
+    <EditorSection>
+      <svelte:fragment slot="title">Items</svelte:fragment>
+      <ItemsEditor />
+    </EditorSection>
+    <EditorSection>
+      <svelte:fragment slot="title">Layout</svelte:fragment>
+      <LayoutEditor />
+    </EditorSection>
   </svelte:fragment>
 </EditorLayout>
---
+++ src/lib/editor/EditorSection.svelte
@@ -0,0 +1,10 @@
+<div class="card m-4 rounded-md bg-base-100 shadow-2xl">
+  <div class="card-body">
+    {#if $$slots.title}
+      <h2 class="card-title">
+        <slot name="title" />
+      </h2>
+    {/if}
+    <slot />
+  </div>
+</div>

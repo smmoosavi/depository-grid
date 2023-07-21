6fad8675d90961a92f39b462c61e15a20b5f0a51
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2023-07-22 01:28:05 +0330

    add serialize and label functions to items

--- src/lib/editor/state/items.ts
+++ src/lib/editor/state/items.ts
@@ -1,4 +1,5 @@
 import { nanoid } from 'nanoid';
+import { itemsToString } from './pattern/pattern';
 import type { Pattern } from './pattern/pattern';
 
 export type ItemId = string;
@@ -61,4 +62,71 @@ export function table(table: TableLabel, rows: RowPattern, cells: CellPattern): 
 export function group(tables: TablePattern, rows: RowPattern, cells: CellPattern): Group {
   const id = nanoid(6);
   return { kind: 'group', id, tables, rows, cells };
+}
+
+export function serializeCell(cell: Cell): string {
+  return `${cell.table}/${cell.row}/${cell.cell}`;
+}
+
+export function serializeRow(row: Row): string {
+  const cells = itemsToString(row.cells.items);
+  return `${row.table}/${row.row}/${cells}`;
+}
+
+export function serializeTable(table: Table): string {
+  const rows = itemsToString(table.rows.items);
+  const cells = itemsToString(table.cells.items);
+  return `${table.table}/${rows}/${cells}`;
+}
+
+export function serializeGroup(group: Group): string {
+  const tables = itemsToString(group.tables.items);
+  const rows = itemsToString(group.rows.items);
+  const cells = itemsToString(group.cells.items);
+  return `${tables}/${rows}/${cells}`;
+}
+
+export function serializeItem(item: Item): string {
+  switch (item.kind) {
+    case 'cell':
+      return serializeCell(item);
+    case 'row':
+      return serializeRow(item);
+    case 'table':
+      return serializeTable(item);
+    case 'group':
+      return serializeGroup(item);
+  }
+}
+
+export function cellLabel(cell: Cell): string {
+  return `${cell.table}/${cell.row}/${cell.cell}`;
+}
+
+export function rowLabel(row: Row): string {
+  return `${row.table}/${row.row}`;
+}
+
+export function tableLabel(table: Table): string {
+  return `${table.table}`;
+}
+
+export function groupLabel(group: Group): string {
+  const tables = itemsToString(group.tables.items);
+  const rows = itemsToString(group.rows.items);
+  const cells = itemsToString(group.cells.items);
+  return `${tables}/${rows}/${cells}`;
+}
+
+export function itemLabel(item: Item): string {
+  switch (item.kind) {
+    case 'cell':
+      return cellLabel(item);
+    case 'row':
+      return rowLabel(item);
+    case 'table':
+      return tableLabel(item);
+    case 'group':
+      return groupLabel(item);
+  }
 }

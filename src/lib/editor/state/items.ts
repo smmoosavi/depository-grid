21e2bcfcb43d5fbe060588f4096ee69838d80623
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2023-07-27 10:12:12 +0330

    remove cell/row/table types

--- src/lib/editor/state/items.ts
+++ src/lib/editor/state/items.ts
@@ -11,30 +11,6 @@ export type CellPattern = Pattern<CellLabel>;
 export type RowPattern = Pattern<RowLabel>;
 export type TablePattern = Pattern<TableLabel>;
 
-export type Cell = {
-  kind: 'cell';
-  id: ItemId;
-  table: TableLabel;
-  row: RowLabel;
-  cell: CellLabel;
-};
-
-export type Row = {
-  kind: 'row';
-  id: ItemId;
-  table: TableLabel;
-  row: RowLabel;
-  cells: CellPattern;
-};
-
-export type Table = {
-  kind: 'table';
-  id: ItemId;
-  table: TableLabel;
-  rows: RowPattern;
-  cells: CellPattern;
-};
-
 export type Group = {
   kind: 'group';
   id: ItemId;
@@ -43,40 +19,11 @@ export type Group = {
   cells: CellPattern;
 };
 
-export type Item = Cell | Row | Table | Group;
-export function cell(table: TableLabel, row: RowLabel, cell: CellLabel): Cell {
-  const id = nanoid(6);
-  return { kind: 'cell', id, table, row, cell };
-}
-
-export function row(table: TableLabel, row: RowLabel, cells: CellPattern): Row {
-  const id = nanoid(6);
-  return { kind: 'row', id, table, row, cells };
-}
-
-export function table(table: TableLabel, rows: RowPattern, cells: CellPattern): Table {
-  const id = nanoid(6);
-  return { kind: 'table', id, table, rows, cells };
-}
+export type Item = Group;
 
 export function group(tables: TablePattern, rows: RowPattern, cells: CellPattern): Group {
   const id = nanoid(6);
   return { kind: 'group', id, tables, rows, cells };
-}
-
-export function serializeCell(cell: Cell): string {
-  return `${cell.table}/${cell.row}/${cell.cell}`;
-}
-
-export function serializeRow(row: Row): string {
-  const cells = itemsToString(row.cells.items);
-  return `${row.table}/${row.row}/${cells}`;
-}
-
-export function serializeTable(table: Table): string {
-  const rows = itemsToString(table.rows.items);
-  const cells = itemsToString(table.cells.items);
-  return `${table.table}/${rows}/${cells}`;
 }
 
 export function serializeGroup(group: Group): string {
@@ -88,27 +35,9 @@ export function serializeGroup(group: Group): string {
 
 export function serializeItem(item: Item): string {
   switch (item.kind) {
-    case 'cell':
-      return serializeCell(item);
-    case 'row':
-      return serializeRow(item);
-    case 'table':
-      return serializeTable(item);
     case 'group':
       return serializeGroup(item);
   }
-}
-
-export function cellLabel(cell: Cell): string {
-  return `${cell.table}/${cell.row}/${cell.cell}`;
-}
-
-export function rowLabel(row: Row): string {
-  return `${row.table}/${row.row}`;
-}
-
-export function tableLabel(table: Table): string {
-  return `${table.table}`;
 }
 
 export function groupLabel(group: Group): string {
@@ -120,12 +49,6 @@ export function groupLabel(group: Group): string {
 
 export function itemLabel(item: Item): string {
   switch (item.kind) {
-    case 'cell':
-      return cellLabel(item);
-    case 'row':
-      return rowLabel(item);
-    case 'table':
-      return tableLabel(item);
     case 'group':
       return groupLabel(item);
   }

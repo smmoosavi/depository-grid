7bf4e59b06bcbe91b6137b6db7fb1b67b3bce4fc
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2023-08-18 01:20:51 +0330

    improve cell view

--- src/lib/editor/preview/CellPreview.svelte
+++ src/lib/editor/preview/CellPreview.svelte
@@ -4,14 +4,8 @@
   export let cell: Cell;
 </script>
 
-<div class="flex h-full items-center justify-center gap-1 overflow-hidden font-extrabold">
-  <span class="inline-block">
-    {cell.table}
-  </span>
-  <span class="inline-block">
-    {cell.row}
-  </span>
-  <span class="inline-block">
-    {cell.cell}
-  </span>
+<div class="flex h-full items-center justify-center overflow-hidden font-extrabold">
+  &#x202D;{cell.table}
+  {cell.row}
+  {cell.cell}
 </div>

c2d6e33cb436bd1c79c4164ce080d4a55c121706
Seyyed Morteza Moosavi <smmoosavi@users.noreply.github.com>
2024-07-14 11:08:15 +0330

    update dependencies (#1)
    * remove storybook
    
    * update svelte
    
    * update postcss/tailwind/daisyui
    
    * fix test
    
    * update runtime deps
    
    * update dev dependencies

--- src/lib/utils/unwrap.ts
+++ src/lib/utils/unwrap.ts
@@ -1,5 +1,8 @@
 export class UnwrapError<T> extends Error {
-  constructor(message: string, public data: T) {
+  constructor(
+    message: string,
+    public data: T,
+  ) {
     super(message);
     this.name = 'UnwrapError';
     Object.setPrototypeOf(this, UnwrapError.prototype);
